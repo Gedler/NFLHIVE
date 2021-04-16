5aa300900b53f603816708f11084168b08c92435
iaconom821 <iaconom821@gmail.com>
2021-04-16 14:27:00 -0400

    add teams and events pages

--- src/components/Events.js
+++ src/components/Events.js
@@ -1,18 +1,43 @@
+import React, { useState, useEffect } from 'react'
 import EventForm from "./EventForm";
 
 
+function Events({ team }){
+    const [events, setEvents] = useState([])
 
+    useEffect( () => {
+        fetch(`http://localhost:4000/upcomingEvents`)
+        .then(res => res.json())
+        .then(eventsArray => setEvents(eventsArray))
+    }, [])
 
+    const filterEventsList = events.filter(event=> {
+        return event.teamsId === team.id
+    })
 
-function Events(){
+    const eventsList = filterEventsList.map( event => {
+        return (
+            <div key={ event.id }>
+                <span> 
+                    <p>{event.title}</p>
+                    <p>{event.date}</p>
+                    <p>{event.time}</p>
+                    <p>{event.opponent}</p>
+                    <p>{event.location}</p>
+                    <p>{event.comments}</p>
+                </span>
+            </div>
+        )
+    })
     console.log("Events")
 
 
-return ( 
-    <div>
-    <EventForm/>
-    </div>
-)
+    return ( 
+        <div>
+        <EventForm/>
+        { eventsList}
+        </div>
+    )
 
 }
 
--- src/components/TitleBanner.js
+++ src/components/TitleBanner.js
@@ -1,17 +1,10 @@
 function TitleBanner() {
     console.log("Hello, World!")
-
-
-
         return ( 
             <div>
                 <h1>NFLHIVE</h1>
             </div>
         )
-
-
-
-
 }
 
 
