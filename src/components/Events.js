00e7e624c7916691d7b7bbf6953b85aed2a3ff51
Gedler <gedler.jeanpierre@stonybrook.edu>
2021-04-19 15:12:41 -0400

    Added Delete Button/Errors with state

--- src/components/Events.js
+++ src/components/Events.js
@@ -5,26 +5,44 @@ import EventForm from "./EventForm";
 function Events({ selectedTeam }){
     const [events, setEvents] = useState([])
 
+
+function deleteEventPost(e) {
+    fetch(`http://localhost:4000/upcomingEvents/${e.target.name}`, {
+        method: "delete", })
+        let filteredDelete = events.filter(event => {
+            // console.log(e.target.name)
+           
+            return e.target.name !== event.id
+
+        })
+        setEvents(filteredDelete)
+         console.log(e.target.name)
+        
+
+    
+    
+}
     useEffect( () => {
         fetch(`http://localhost:4000/upcomingEvents`)
         .then(res => res.json())
         .then(eventsArray => setEvents(eventsArray))
     }, [])
 
-    const filterEventsList = events.filter(event=> {
-        return event.teamsId === selectedTeam.id
+    const filterEventsList = events.filter(event=> { // Filtering the upComingEvents API
+        return event.teamsId === selectedTeam.id  // return the teams that match the same ID from the teamsAPI. TeamsAPI was imported as a prop.
     })
 
-    const eventsList = filterEventsList.map( event => {
+    const eventsList = filterEventsList.map( event => {  // return the teams that match the teamsAPI for ALL TEAMS.
         return (
             <div key={ event.id }>
                 <span> 
-                    <p>{event.title}</p>
+                    <p>{event.title}</p>   
                     <p>{event.date}</p>
                     <p>{event.time}</p>
                     <p>{event.opponent}</p>
                     <p>{event.location}</p>
                     <p>{event.comments}</p>
+                    <button name= {event.id} onClick= {deleteEventPost}>Delete</button>
                 </span>
             </div>
         )
